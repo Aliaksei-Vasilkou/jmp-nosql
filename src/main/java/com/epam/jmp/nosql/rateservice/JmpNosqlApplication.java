6b165064da72a6c7f419f4b1fdcdf26c899f2717
Aliaksei Vasilkou <Aliaksei.Vasilkou@ihg.com>
2023-08-15 17:20:41 +0200

    initial commit

---
+++ src/main/java/com/epam/jmp/nosql/rateservice/JmpNosqlApplication.java
@@ -0,0 +1,13 @@
+package com.epam.jmp.nosql.rateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmpNosqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmpNosqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/jmp/nosql/rateservice/JmpNosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.jmp.nosql.rateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmpNosqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
